b31468f42e58f22b8f80a33f5b804f97d8f5d558
Wess Cope <wcope@me.com>
2019-07-17 17:37:57 -0400

    revert version

--- src/commands/train.ts
+++ src/commands/train.ts
@@ -6,26 +6,16 @@
 //  Copyright 2019 Skafos,llc.
 //
 
-import {flags} from '@oclif/command';
 import Command from './base';
 
 export default class Train extends Command {
   static description = "Run model and train it with provided data source";
 
   static examples = [
-    `$ pgo train --data=/path/to/example`
+    `$ pgo train`
   ]
 
-  static flags = {
-    ...Command.flags,
-    help: flags.help({char: 'h'})
-  }
-
-  static args = [{
-    name: 'data'
-  }]
-
   async run() {
-    
+    console.log("TRAIN")    
   }
 }
